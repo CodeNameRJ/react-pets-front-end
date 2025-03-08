3cbcb5ce731e0c4363257ee678db23bc8c7304e8
Rjcode <jdrandy24@gmail.com>
2025-03-07 20:05:50 -0500

    getpets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import { index } from './services/petService';
 
 
 const App = () => {
+
+
+  const getPets = async () => {
+    console.log(await index())
+  }
+
+  useEffect(() => {
+    getPets()
+  }, [])
+
+
+
   return <h1>Hello world!</h1>;
 };
 
---
+++ src/services/petService.js
@@ -0,0 +1,23 @@
+const BASE_URL=`${import.meta.env.VITE_BACK_END_SERVER_URL}/pets`  // import to be able to access variable for use in all fetch
+
+
+export const index = async () => {
+    try {
+        const res = await fetch(BASE_URL) // make request to base URL using fetch
+        return res.json() // to resolve response need to invoke json
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+// export { // creating an object with key and value of the function {index: index}
+//     index
+// }
+
+
+// or you can put export before variable declaration
+// this is called a name export
+
+//default export is differet - instead of exproting an object of key value pairs, by default its exporting whole function so you can import it as somethingelse from
+// importing name is destricted { }
